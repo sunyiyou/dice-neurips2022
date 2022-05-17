c198d10aeab20490a84e8d3c935c8a38951524ad
Yiyou Sun <342065733@qq.com>
2022-05-17 18:19:55 -0500

    Create download_ood.sh

---
+++ download_ood.sh
@@ -0,0 +1,3 @@
+wget http://pages.cs.wisc.edu/~huangrui/imagenet_ood_dataset/iNaturalist.tar.gz
+wget http://pages.cs.wisc.edu/~huangrui/imagenet_ood_dataset/SUN.tar.gz
+wget http://pages.cs.wisc.edu/~huangrui/imagenet_ood_dataset/Places.tar.gz
